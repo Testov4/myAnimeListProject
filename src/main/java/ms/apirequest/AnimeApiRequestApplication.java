f36f7c0de7c6ddbd8e36f08a5d0ac9ef8ba16cab
Testov4 <itiestov@mail.ru>
2023-09-03 20:02:07 +0300

    2

--- src/main/java/ms/apirequest/AnimeApiRequestApplication.java
+++ src/main/java/ms/apirequest/AnimeApiRequestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class AnimeApiRequestApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AnimeApiRequestApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeApiRequestApplication.class, args);
     }
 }
