efe09a654c45df3f127d937c0e7b4fb812ad3309
Testov4 <35634384+Testov4@users.noreply.github.com>
2023-09-09 22:20:50 +0300

    Revert "Dev"

--- src/main/java/ms/apirequest/exception/WrongRequestFormatException.java
+++
@@ -1,7 +0,0 @@
-package ms.apirequest.exception;
-
-public class WrongRequestFormatException extends RuntimeException{
-    public WrongRequestFormatException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ms/apirequest/service/RequestDeserializerService.java
+++
@@ -1,8 +0,0 @@
-package ms.apirequest.service;
-
-import java.util.Map;
-
-public interface RequestDeserializerService {
-
-    Map<String, Object> deserializeAnimeSearchRequest(String json);
-}
